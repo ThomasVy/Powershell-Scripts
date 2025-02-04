2da1e0d9fd2c13b4455f8f118c0760ae3860d90c
ThomasVy <vythomas97@gmail.com>
2025-02-03 19:35:59 -0700

    better installation steps

--- install.ps1
+++ install.ps1
@@ -1,28 +1,34 @@
-Write-Host -ForegroundColor Blue install better ls (eza)
+Write-Host -ForegroundColor Blue "install better ls (eza)"
 winget install eza-community.eza
 
-Write-Host -ForegroundColor Blue install better cat (bat) 
+Write-Host -ForegroundColor Blue "install better cat (bat)"
 winget install sharkdp.bat
 
-Write-Host -ForegroundColor Blue install better grep (ripgrep)
-winget install burntsushi.ripgrep
+Write-Host -ForegroundColor Blue "install better grep (ripgrep)"
+winget install BurntSushi.ripgrep.MSVC
 
-Write-Host -ForegroundColor Blue install better find (fd)
+Write-Host -ForegroundColor Blue "install power toys"
+winget install --id Microsoft.PowerToys --source winget
+
+Write-Host -ForegroundColor Blue "install better find (fd)"
 winget install sharkdp.fd
 
-Write-Host -ForegroundColor Blue install neovim
+Write-Host -ForegroundColor Blue "install neovim"
 winget install neovim.neovim
 
-Write-Host -ForegroundColor Blue install better cd (zoxide)
+Write-Host -ForegroundColor Blue "install better cd (zoxide)"
 winget install ajeetdsouza.zoxide
 
-Write-Host -ForegroundColor Blue install better htop (bottom)
-winget install clementtsang.bottom
+Write-Host -ForegroundColor Blue "install better htop (bottom)"
+winget install bottom
 
-Write-Host -ForegroundColor Blue install better fuzzy finder (fzf)
+Write-Host -ForegroundColor Blue "install better fuzzy finder (fzf)"
 winget install fzf
 
-Write-Host -ForegroundColor Blue install OhMyPosh
+Write-Host -ForegroundColor Blue "install Windows Terminal"
+winget install --id Microsoft.WindowsTerminal -e
+
+Write-Host -ForegroundColor Blue "install OhMyPosh"
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
 oh-my-posh font install meslo
--- installAdmin.ps1
+++ installAdmin.ps1
@@ -1,4 +1,5 @@
-Install-Module -Name PSReadLine
+Install-Module -Name PSReadLine -Force
 
-Install-Module -Name PSFzf
+Install-Module -Name PSFzf -Force
 
+Install-Module -Name PowerShellGet -Force
