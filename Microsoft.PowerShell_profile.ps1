8d32189eb05d024dd3e125889b126e19f44d2796
Thomas Vy <vythomas97@gmail.com>
2025-12-26 18:33:56 -0700

    adds the current working directory to powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -125,6 +125,17 @@ function Sllogin {
         Start-Process $Url
     }
 }
-
 # Define an alias for the specific role and account
 Set-Alias -Name onprem-test-eng -Value Sllogin
+
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
+
