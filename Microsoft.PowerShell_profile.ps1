64d4780151adf7b92fd971f00975e6197e7d0947
Thomas Vy <vythomas97@gmail.com>
2025-12-26 18:50:41 -0700

    oh my posh will open in the same directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -128,14 +128,4 @@ function Sllogin {
 # Define an alias for the specific role and account
 Set-Alias -Name onprem-test-eng -Value Sllogin
 
-function prompt {
-  $loc = $executionContext.SessionState.Path.CurrentLocation;
-
-  $out = ""
-  if ($loc.Provider.Name -eq "FileSystem") {
-    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-  }
-  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-  return $out
-}
 
